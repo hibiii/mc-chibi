c9666a8963451027cd349b1617e398cbdf1070ea
Hibiii <69405847+Hibiii@users.noreply.github.com>
2020-10-27 19:18:22 -0300

    Add translation support

--- src/main/java/hibiii/hibex/HibexModmenu.java
+++ src/main/java/hibiii/hibex/HibexModmenu.java
@@ -4,7 +4,8 @@ import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
-import net.minecraft.text.*;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
 public class HibexModmenu implements ModMenuApi {
 	// Sensible19
@@ -25,12 +26,21 @@ public class HibexModmenu implements ModMenuApi {
 			
 			ConfigBuilder builder = ConfigBuilder.create();
 			ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-			builder.setParentScreen(parent).setTitle(Text.of("title.hibex.config"));
-			builder.getOrCreateCategory(Text.of("category.examplemod.general"))
+			builder.setParentScreen(parent).setTitle(new TranslatableText("hibex.menu.title"));
+			builder.getOrCreateCategory(new TranslatableText("hibex.menu.general"))
 			.addEntry(entryBuilder
-					.startEnumSelector(Text.of("options.hibex.weaponSwingThreshold"),WeaponSwingControl.class,weaponSwingThreshold)
+					.startEnumSelector(new TranslatableText("hibex.option.weapon_swing_threshold"),WeaponSwingControl.class,weaponSwingThreshold)
 					.setDefaultValue(WeaponSwingControl.NONE)
 					.setSaveConsumer(newValue -> weaponSwingThreshold = newValue)
+					.setEnumNameProvider(enumKey -> {
+						switch ((WeaponSwingControl)enumKey) {
+							case NONE: return new TranslatableText("hibex.option.weapon_swing_threshold.none");
+							case LENIENT: return new TranslatableText("hibex.option.weapon_swing_threshold.lenient");
+							case STRICT: return new TranslatableText("hibex.option.weapon_swing_threshold.strict");
+						}
+						return Text.of("SwitchError");})
+					.build())
+			.addEntry(entryBuilder.startTextDescription(new TranslatableText("hibex.option.weapon_swing_threshold.warning"))
 					.build());
 			return builder.build();
 		};
