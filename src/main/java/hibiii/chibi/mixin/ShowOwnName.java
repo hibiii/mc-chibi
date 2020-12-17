837bad9a2c789996803460e4c329af8eb02164ef
Hibiii <69405847+Hibiii@users.noreply.github.com>
2020-12-17 13:26:18 -0300

    Add the ability to show own name in third person

---
+++ src/main/java/hibiii/chibi/mixin/ShowOwnName.java
@@ -0,0 +1,29 @@
+package hibiii.chibi.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.entity.LivingEntity;
+
+@Mixin(LivingEntityRenderer.class)
+public abstract class ShowOwnName<T extends LivingEntity, M extends EntityModel<T>> extends EntityRenderer<T> implements FeatureRendererContext<T, M> {
+
+	protected ShowOwnName(EntityRenderDispatcher dispatcher) {
+		super(dispatcher);
+	}
+
+	@Inject(method = "hasLabel", at = @At("HEAD"), cancellable = true)
+	public void showOwnName(T livingEntity, CallbackInfoReturnable<Boolean> cir) {
+		MinecraftClient client = MinecraftClient.getInstance();
+		if (livingEntity == client.cameraEntity)
+			cir.setReturnValue(true);
+	}
+}
